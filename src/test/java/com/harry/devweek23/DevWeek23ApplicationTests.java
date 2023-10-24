14d4a178df60ec6c6150fd023cb709cf01323535
Harrison de Oliveira Rodrigues <95246993+Harrison1033@users.noreply.github.com>
2023-10-23 21:00:42 -0300

    Initial commit

---
+++ src/test/java/com/harry/devweek23/DevWeek23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harry.devweek23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeek23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
